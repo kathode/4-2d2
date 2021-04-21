1866a20ff53f96b326dee0d9ba82e763ffee4a7d
kathode <80877706+kathode@users.noreply.github.com>
2021-04-21 18:45:45 +1000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,8 @@ const IndexPage = () => (
   <Layout>
     <Seo title="Remote Traffic Lights Control" />
     <h1>Traffic Lights</h1>
-    
+    <button class ="button button1">Green</button>
+    <button class ="button button2">Green</button>
   </Layout>
 )
 
