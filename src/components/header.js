31b9ba7ba47c2336e77d694a6dd87f0b1a6dce9e
kathode <80877706+kathode@users.noreply.github.com>
2021-04-21 17:58:25 +1000

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import { Link } from "gatsby"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `black`,
       marginBottom: `1.45rem`,
     }}
   >
@@ -36,7 +36,7 @@ Header.propTypes = {
 }
 
 Header.defaultProps = {
-  siteTitle: ``,
+  siteTitle: `X`,
 }
 
 export default Header
